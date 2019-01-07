2768145acf74e4dd243ef096c2a2450c1bf5acb6
Gunn <johngunn@88e9fe624695.ant.amazon.com>
2019-01-07 07:16:20 -0500

    fundtioning fill pot intent; invocation name set to my Kitchen

--- lambda/custom/index.js
+++ lambda/custom/index.js
@@ -2,6 +2,7 @@
 /* eslint-disable  no-console */
 
 const Alexa = require('ask-sdk-core');
+const AWS = require('aws-sdk');
 
 const LaunchRequestHandler = {
   canHandle(handlerInput) {
@@ -33,19 +34,91 @@ const HelloWorldIntentHandler = {
   },
 };
 
-const OrderPizzaIntentHandler = {
+const FillPotIntentHandler = {
   canHandle(handlerInput) {
     return handlerInput.requestEnvelope.request.type === 'IntentRequest'
-      && handlerInput.requestEnvelope.request.intent.name === 'OrderPizzaIntent';
+      && handlerInput.requestEnvelope.request.intent.name === 'FillPotIntent';
   },
   handle(handlerInput) {
-    const speechText = 'I have ordered your pizza. It should arrive in 30 mins or less';
+    const speechText = 'OK.  Filling your coffee pot now.';
 
+    // send message to S3 topic to prompt fill
+    const SNS = new AWS.SNS({ apiVersion: '2010-03-31' });
+    const DEST_SNS_TOPIC = "cuppyCall";
+
+    // get SNS topic ARN based on topic name
+    const createParams = {
+      Name: DEST_SNS_TOPIC
+    };
+/*
+  console.log ('about to create topic basd on name.  Params:');
+  console.log(createParams);
+  // Create promise and SNS service object
+  var createTopicPromise = SNS.createTopic(createParams).promise();
+
+  // Handle promise's fulfilled/rejected states
+  createTopicPromise.then(
+    function(data) {
+      console.log("Topic ARN is " + data.TopicArn);
+    }).catch(
+      function(err) {
+      console.error(err, err.stack);
+    });
+
+
+
+    console.log ('about to create topic basd on name.  Params:');
+    console.log(createParams);
+    var getTopicPromise = SNS.createTopic(createParams).promise();
+    console.log ('now have the get topic promise');
+
+    // Handle promise's fulfilled/rejected states
+    getTopicPromise.then(
+      function(data) {
+        console.log('Get topic promise fulfilled.  Now capture resulting TpicArn');
+        const topicArn = data.TopicArn;
+        console.log(`Created topic: ${topicArn}`);
+      }).catch(
+        function(err) {
+          console.log('Creating topic failed.', err);
+          return handlerInput.responseBuilder
+            .speak('Sorry, I wasn\'t able to communicate with the coffee pot .')
+            .reprompt('Sorry, I can\'t understand the command. Please say again.')
+            .getResponse();
+      });
+     */ 
+    // Create publish parameters
+    const pubParams = {
+      Message: "20",
+      Subject: `Invocation from AlexaSkill HelloWorld: FillPotIntent`,
+      TopicArn: `arn:aws:sns:us-east-1:326542932491:cuppyCall`,
+    };
+
+    // publish to SNS topic
+    console.log('About to publish to SNS. params:');
+    console.log(pubParams);
+    var publishTextPromise = SNS.publish(pubParams).promise();
+
+    // Handle promise's fulfilled/rejected states
+    publishTextPromise.then(
+      function(data) {
+        console.log(`Message ${pubParams.Message} send sent to the topic ${pubParams.TopicArn}`);
+        console.log("MessageID is " + data.MessageId);
+      }).catch(
+        function(err) {
+          console.log(`Failed sending to the topic ${pubParams.TopicArn}`, err);
+          return handlerInput.responseBuilder
+            .speak('Sorry, I wasn\'t able to communicate with the coffee pot .')
+            .reprompt('Sorry, I can\'t understand the command. Please say again.')
+            .getResponse();
+      });
+        
     return handlerInput.responseBuilder
       .speak(speechText)
-      .withSimpleCard('Order Pizza', speechText)
+      .withSimpleCard('Fill Pot', speechText)
       .getResponse();
   },
+
 };
 
 const HelpIntentHandler = {
@@ -111,10 +184,11 @@ exports.handler = skillBuilder
   .addRequestHandlers(
     LaunchRequestHandler,
     HelloWorldIntentHandler,
-    OrderPizzaIntentHandler,
+    FillPotIntentHandler,
     HelpIntentHandler,
     CancelAndStopIntentHandler,
     SessionEndedRequestHandler
   )
+
   .addErrorHandlers(ErrorHandler)
   .lambda();
